5b61826b90a467edccf017582127347f3f35feac
Josh-Torre <144864285+Josh-Torre@users.noreply.github.com>
2023-11-24 23:37:53 -0500

    working page with splash and navigaton bar

--- illusion.js
+++ illusion.js
@@ -16,21 +16,22 @@ const el = document.getElementById('illusion1');
 
     console.log(ballWidth);
   }
-    //Updates mouse coordinates   
-    function updateMouseCoordinates(event) {
-        //Get the current position of the canvasd
-        var position = el.getBoundingClientRect();
-        var divX = position.left;
-        var divY = Math.ceil(position.top);
 
-        //set the position of the mouse relative to the div
-        mouseX = event.clientX - divX;
-        mouseY = event.clientY - divY;
+  //Updates mouse coordinates   
+  function updateMouseCoordinates(event) {
+      //Get the current position of the canvasd
+      var position = el.getBoundingClientRect();
+      var divX = position.left;
+      var divY = Math.ceil(position.top);
 
-        updateBallPosition();
+      //set the position of the mouse relative to the div
+      mouseX = event.clientX - divX;
+      mouseY = event.clientY - divY;
 
-        console.log(`Mouse Coordinates: (${mouseX}, ${mouseY})`);
-    }
+      updateBallPosition();
 
-    // Attach mousemove event listener to track mouse movements
-    el.addEventListener("mousemove", updateMouseCoordinates);
+      console.log(`Mouse Coordinates: (${mouseX}, ${mouseY})`);
+  }
+
+  // Attach mousemove event listener to track mouse movements
+el.addEventListener("mousemove", updateMouseCoordinates);
