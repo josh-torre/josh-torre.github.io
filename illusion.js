b795659c60b366db0ad613d886ac4b9b24a9fce9
Josh-Torre <144864285+Josh-Torre@users.noreply.github.com>
2023-12-06 01:25:31 -0500

    Added secondary illusion and removed link from home.

--- illusion.js
+++ illusion.js
@@ -1,12 +1,11 @@
-//    <!--#19048---#7752FE---#8E8FFA---#C2D9FF-->
-
 let mouseX;
 let mouseY;
 
-  const el = document.getElementById('outer-shell');
+const el1 = document.getElementById('illusion1-outer-shell');
+const el2 = document.getElementById('illusion2-outer-shell');
 
-  function updateBallPosition(){
-    var ball = document.getElementById("ball");
+  function updateBallPosition(ballID){
+    var ball = document.getElementById(ballID);
     const ballAttributes = getComputedStyle(ball);
     var ballWidth = Number.parseInt(ballAttributes.width.substring(0, ballAttributes.width.indexOf("p")));
     var ballHeight  = Number.parseInt(ballAttributes.height.substring(0, ballAttributes.height.indexOf("p")));
@@ -16,18 +15,32 @@ let mouseY;
   }
 
   //Updates mouse coordinates   
-  function updateMouseCoordinates(event) {
-      //Get the current position of the canvasd
-      var position = el.getBoundingClientRect();
-      var divX = position.left;
-      var divY = Math.ceil(position.top);
+  function updateMouseCoordinatesEL1(event) {
+    //Get the current position of the canvasd
+    var position = el1.getBoundingClientRect();
+    var divX = position.left;
+    var divY = Math.ceil(position.top);
 
-      //set the position of the mouse relative to the div
-      mouseX = event.clientX - divX;
-      mouseY = event.clientY - divY;
+    //set the position of the mouse relative to the div
+    mouseX = event.clientX - divX;
+    mouseY = event.clientY - divY;
 
-      updateBallPosition();
-  }
+    updateBallPosition("ball-il1");
+}
+
+  //Updates mouse coordinates   
+  function updateMouseCoordinatesEL2(event) {
+    //Get the current position of the canvasd
+    var position = el2.getBoundingClientRect();
+    var divX = position.left;
+    var divY = Math.ceil(position.top);
+
+    //set the position of the mouse relative to the div
+    mouseX = event.clientX - divX;
+    mouseY = event.clientY - divY;
+
+    updateBallPosition("ball-il2");
+}
 
   function getColorArray(canvasArray){
     for(i = 0; i < canvasArray.data.length; i+=4){
@@ -38,7 +51,8 @@ let mouseY;
       canvasArray.data[i + 3] = 255/2;
     }
   }
-  function makeCanvasBackground(id){
+  
+  function makeCanvasBackgroundBinary(id){
     var canvas = document.getElementById(id);
     var context = canvas.getContext("2d");
 
@@ -48,17 +62,42 @@ let mouseY;
       const randomChance = Math.random();
       var colorScalar = 0;
       if(randomChance <= 0.2)
-        colorScalar = 0
+        colorScalar = 0;
       else
-        colorScalar = 1
+        colorScalar = 1;
 
       canvasArray.data[i] = colorScalar * 255;  // red   color
       canvasArray.data[i + 1] = colorScalar * 255;  // green color
       canvasArray.data[i + 2] = colorScalar * 255;  // blue  color
       if(colorScalar == 1)
-      canvasArray.data[i + 3] = 0;
+        canvasArray.data[i + 3] = 0;
       else
-      canvasArray.data[i + 3] =255;
+        canvasArray.data[i + 3] =255;
+    }
+    canvas.style.width = "450 px";
+    canvas.style.height = "450 px";
+    canvasArray.height = 20;
+    canvasArray.width = 20;
+    context.putImageData(canvasArray, 0, 0);
+  }
+
+  function makeCanvasBackground255(id){
+    var canvas = document.getElementById(id);
+    var context = canvas.getContext("2d");
+
+    var canvasArray = context.createImageData(300, 300);
+
+    for(i = 0; i < canvasArray.data.length; i+=4){
+      const randomChance = Math.random();
+      var colorScalar = randomChance;
+     /* if(randomChance <= 0.2)
+        colorScalar = 0
+      else
+        colorScalar = 1*/
+      canvasArray.data[i] = colorScalar * 255;  // red   color
+      canvasArray.data[i + 1] = colorScalar * 255;  // green color
+      canvasArray.data[i + 2] = colorScalar * 255;  // blue  color
+      canvasArray.data[i + 3] =255*(1-colorScalar);
     }
     canvas.style.width = "450 px";
     canvas.style.height = "450 px";
@@ -68,6 +107,12 @@ let mouseY;
   }
 
     // Attach mousemove event listener to track mouse movements
-    el.addEventListener("mousemove", updateMouseCoordinates);
-    makeCanvasBackground("illusion1-canvas");
-    makeCanvasBackground("illusion1-canvas-ball");
+    el1.addEventListener("mousemove", updateMouseCoordinatesEL1);
+    el2.addEventListener("mousemove", updateMouseCoordinatesEL2);
+
+    makeCanvasBackgroundBinary("illusion1-canvas");
+    makeCanvasBackgroundBinary("illusion1-canvas-ball");
+
+    makeCanvasBackground255("illusion2-canvas");
+    makeCanvasBackground255("illusion2-canvas-ball");
+
