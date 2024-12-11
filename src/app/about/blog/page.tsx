704a74d696f3023177230060d554b69c8aa7f286
jtorre <torre.jo@northeastern.edu>
2024-12-11 14:35:31 -0500

    Added post

---
+++ src/app/about/blog/page.tsx
@@ -0,0 +1,14 @@
+import TitleCard from "@/app/Core Components/PageTitle"
+import Link from 'next/link'
+
+export default function AboutMe() {
+  return (
+      <div className="h-full w-full min-h-screen max-h-full bg-my-dark-blue p-6">
+          <header className="w-full flex flex-col sm:flex-row m-auto">
+              <TitleCard title={"My Blog"}/>
+          </header>
+          <Link href="blog/llms-in-cs">Dashboard</Link>
+        </div>
+ )
+}
+ 
